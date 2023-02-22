856adcea56b43da493c851c56e635fa12d860ffe
Arpit150321 <114680435+Arpit150321@users.noreply.github.com>
2023-02-22 12:22:59 +0530

    Add files via upload

---
+++ GitHub/src/Leetcode/Sorting/Essay/Q349_Intersection_of_Two_Arrays.java
@@ -0,0 +1,13 @@
+package Leetcode.Sorting.Essay;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Q349_Intersection_of_Two_Arrays {
+    public static void main(String[] args) {
+        int[] nums1 = {4,9,5};
+        int[] nums2 = {9,4,9,8,4};
+        Solution s = new Solution();
+        System.out.println(Arrays.toString(s.intersection(nums1,nums2)));
+    }
+}
